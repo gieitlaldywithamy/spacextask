4da5ddef02ea3e06d0e51035a5b716bbcff4cc8d
Amy Morrison <amy.jeanette.morrison@gmail.com>
2021-03-13 22:21:08 +0000

    Basic list styling

--- src/components/LaunchListItem/LaunchListItem.styled.jsx
+++ src/components/LaunchListItem/LaunchListItem.styled.jsx
@@ -1,6 +1,18 @@
 import styled from 'styled-components';
 
-export const LaunchInfo = styled.li``;
+export const LaunchInfo = styled.li`
+    width: 92%;
+    margin: 1% 0 1% 1%;
+    padding: 0.5em 1.5em 0.5em 1.5em;
+    list-style-type: none;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    box-shadow: 0 2px 5px 0 rgb(0 0 0 / 25%);
+    border-radius: 10px;
+    font-weight: 500;
+`;
 
 export const FlightNumber = styled.div``;
 
